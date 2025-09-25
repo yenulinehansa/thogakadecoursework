0a2e98ae206f3ff1df644b8808d83cea0b620f15
yenulinehansa <yenulinehansa739@gmail.com>
2025-09-25 13:11:26 +0530

    DBCONNECTION

--- src/main/java/DB/DBConnection.java
+++ src/main/java/DB/DBConnection.java
@@ -1,4 +1,27 @@
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection instance;
+    private final Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade", "root" ,"1234");
+
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        return instance == null ? instance = new DBConnection():instance;
+    }
+
+
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+
 }
